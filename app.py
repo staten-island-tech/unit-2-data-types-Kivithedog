6a495a03c3d13f046aa7d1c2696c971425d96888
Kivithedog <victory27@nycstudents.net>
2024-09-27 09:20:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@
 # print(x,y)
 
 import string
+#do this later
+# print("poop")
+# unless:input==0 print("no poop")
 
-print("poop")
-unless:input==0 print("no poop")
+
+val = [1,2,3,4,5,6,7,8,9,10,300]
+for i in val: 
+    print (val[6])
+    print(val[12])
