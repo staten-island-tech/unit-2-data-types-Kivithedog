868bb24a7609de2b64c97d06e237991ece4190cb
Kivithedog <victory27@nycstudents.net>
2024-09-30 09:21:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ import string
 # unless:input==0 print("no poop")
 
 
-val = [1,2,3,4,5,6,7,8,9,10,300]
-for i in val: 
-    print (val[6])
-    print(val[12])
+# val = [1,2,3,4,5,6,7,8,9,10,300]
+# for i in val: 
+#     print (val[6])
+#     print(val[5])
+
+def tip_calculate():
+    total_price = float(input("enter total bill: $"))
+    tip_precent = float(input("enter tip based on satisfaction: "))
+    tip_amount = ()
+tip_calculate()
