b074a2203a1a7ed2e7f4341c7fbbf1905bb6c3f1
Kivithedog <victory27@nycstudents.net>
2024-10-01 09:21:09 -0400

    tip calc!!

--- app.py
+++ app.py
@@ -13,8 +13,13 @@ import string
 #     print (val[6])
 #     print(val[5])
 
+
 def tip_calculate():
     total_price = float(input("enter total bill: $"))
     tip_precent = float(input("enter tip based on satisfaction: "))
-    tip_amount = ()
+    tip_amount = (tip_precent / 100) * total_price
+    With_tip = tip_amount + total_price
+    print(f"Tip precent: ${tip_precent:.2f}")
+    print(f'total amount (including tip): {With_tip:.2f}')
+
 tip_calculate()
