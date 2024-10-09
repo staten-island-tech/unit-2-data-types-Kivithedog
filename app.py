8dbc0f545f8eda3ec2a86e4741cfb45e77ee30b2
Kivithedog <victory27@nycstudents.net>
2024-10-09 09:23:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,33 @@ import string
 # num_words = len(word)
 # print("number of words in sentence is:",num_words)
 
-number = int(input("input number:"))
-remainder = number % 2
-if remainder == 0:
-    print("even")
-else:
-    print("odd")
+# number = int(input("input number:"))
+# remainder = number % 2
+# if remainder == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+def tip_calculate():
+     
+    tip_percent = {
+        'bad': 0.00,
+        'okay': 0.15,
+        'good': 0.20,
+        'amazing': 0.25
+    }
+        Bill = float(input("enter total bill: $"))
+    
+    service = input("how was your experience?(bad, okay, good, amazing): ")
+
+if service part of tip_percent
+      tip_amount = Bill * tip_percent
+      total_amount = tip_amount + Bill
+
+     total_price = float(input("Enter total bill: $"))
+     print 
+     tip_percent = float(input("bad", "okay", "good", "great"))
+     tip_amount = (tip_percent / 100) * total_price
+     with_tip = tip_amount + total_price
+
+     print(f"Tip: ${tip_amount:.2f}")
